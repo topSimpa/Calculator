3dae03c62ae74fd8fe398fd3da0b9114fec3a92a
topSimpa <mohammedabdulmujeeb16@gmail.com>
2026-04-17 10:51:25 +0100

    wip: Added Calculator Utility Functions
    Added add, subtract, multiply & divide for basic calculator arithmetics

---
+++ calculator.js
@@ -0,0 +1,28 @@
+//Fixed-Program Application
+
+let firstInput;
+let secondInput;
+let operator;
+
+function add(first, second) {
+    const sum  = first + second;
+    return (sum);
+}
+
+
+function subtract(minuend, subtrahend) {
+    const difference = minuend - subtrahend;
+    return (difference);
+}
+
+
+function multiply(factorA, factorB) {
+    const product = factorA * factorB;
+    return (product);
+}
+
+function divide(dividend, divisor) {
+    const quotient = dividend / divisor
+    return (quotient);
+}
+
